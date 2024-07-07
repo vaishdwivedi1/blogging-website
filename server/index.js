9b70334a773aa55db896a73c5a7a1f01b6f983a2
Vaishnavi <114218591+vaishdwivedi1@users.noreply.github.com>
2024-07-07 15:27:02 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,21 +1,70 @@
+// import express from 'express';
+// import dotenv from 'dotenv';
+// import cors from 'cors';
+// import bodyParser from 'body-parser';
+
+// //components
+// import Connection from './database/db.js';
+// import Router from './routes/route.js';
+// import path from "path"
+// import { fileURLToPath } from 'url';
+
+// dotenv.config();
+
+// const app = express();
+
+
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
+// // Serve static files from the React app
+// app.use(express.static(path.join(__dirname, "build")));
+
+// // Catch-all route to handle client-side routing
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "build", "index.html"));
+// });
+
+
+
+// app.use(cors());
+// app.use(bodyParser.json({ extended: true }));
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use('/', Router);
+
+
+// const PORT = 8000;
+// const username = "user";
+// const password = "user";
+
+// Connection(username, password);
+
+// app.listen(PORT, () => console.log(`Server is running successfully on PORT ${PORT}`));
+
+
+
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import bodyParser from 'body-parser';
 
-//components
+// Components
 import Connection from './database/db.js';
 import Router from './routes/route.js';
-import path from "path"
+import path from "path";
 import { fileURLToPath } from 'url';
 
 dotenv.config();
 
 const app = express();
 
-
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+
+app.use(cors());
+app.use(bodyParser.json({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use('/', Router);
+
 // Serve static files from the React app
 app.use(express.static(path.join(__dirname, "build")));
 
@@ -24,17 +73,9 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
-
-
-app.use(cors());
-app.use(bodyParser.json({ extended: true }));
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use('/', Router);
-
-
-const PORT = 8000;
-const username = "user";
-const password = "user";
+const PORT = process.env.PORT || 8000;
+const username = process.env.DB_USERNAME || "user";
+const password = process.env.DB_PASSWORD || "user";
 
 Connection(username, password);
 
